313be763c892fcc6689dd91cd55e6982ecf16c1f
Marcel Friedrich <marcel.friedrich@spreadgroup.com>
2022-12-04 14:05:34 +0100

    Link header to dashboard

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,11 +1,13 @@
 import classes from './Header.module.css';
 import appClasses from '../../App.module.css'
+import { Routes } from '../../Router';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className={`${classes.header} ${appClasses.primary}`}>
       <div className={classes.inner}>
-        <h1>Prepper</h1>
+        <h1><Link to={Routes.DASHBOARD}>Prepper</Link></h1>
       </div>
     </header>
   );
